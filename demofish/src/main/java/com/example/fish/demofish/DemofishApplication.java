0ef9ca2d9bdeacf83289499d28fe135936b56a23
pchai7 <43394505+pchai7@users.noreply.github.com>
2019-04-24 11:40:23 -0400

    demoFish

---
+++ demofish/src/main/java/com/example/fish/demofish/DemofishApplication.java
@@ -0,0 +1,12 @@
+package com.example.fish.demofish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemofishApplication.class, args);
+    }
+}
